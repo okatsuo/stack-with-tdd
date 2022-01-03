4de34f1f6c4c6e0d17c54ae0f09152f58804c53f
okatsuo <rafael.goncanna@gmail.com>
2022-01-02 23:11:53 -0300

    test: ensure the stack is working correctly

--- src/__tests__/stack.spec.ts
+++ src/__tests__/stack.spec.ts
@@ -1,5 +1,40 @@
+import { Stack } from '../stack';
+
 describe('Name of the group', () => {
-  it('should ', () => {
-    
+  const stack = new Stack();
+  const newFirstItem = '🐧'
+  const newSecondItem = '😎'
+
+  it('should create new Stack empty', () => {
+    expect(stack.top).toBe(-1)
+    expect(stack.items).toEqual({})
+  });
+
+  it('should push the first new item to the top', () => {
+    const insertedItem = stack.push(newFirstItem)
+    expect(stack.top).toBe(0)
+    expect(stack.peek()).toBe(newFirstItem)
+    expect(insertedItem).toBe(insertedItem)
+  });
+
+  it('should push the second new item to the top', () => {
+    const insertedItem = stack.push(newSecondItem)
+    expect(stack.top).toBe(1)
+    expect(stack.peek()).toBe(newSecondItem)
+    expect(insertedItem).toBe(insertedItem)
+  });
+
+  it('top should be firstItem if pop with two items in the stack', () => {
+    const removedItem = stack.pop()
+    expect(stack.top).toBe(0)
+    expect(stack.peek()).toBe(newFirstItem)
+    expect(removedItem).toBe(newSecondItem)
+  });
+
+  it('should be empty if remove all items', () => {
+    const removedItem = stack.pop()
+    expect(stack.top).toBe(-1)
+    expect(stack.items).toEqual({})
+    expect(removedItem).toBe(newFirstItem)
   });
 });
--- src/stack/index.ts
+++ src/stack/index.ts
@@ -1,1 +1,26 @@
-console.log('tamo ai sempre junto')
+export class Stack {
+  top = -1
+  items = {}
+
+  push(item: string) {
+    this.top += 1
+    //@ts-expect-error
+    this.items[this.top] = item
+    //@ts-expect-error
+    return this.items[this.top]
+  }
+
+  peek() {
+    //@ts-expect-error
+    return this.items[this.top]
+  }
+
+  pop() {
+    //@ts-expect-error
+    const removedItem = this.items[this.top]
+    //@ts-expect-error
+    delete this.items[this.top]
+    this.top--
+    return removedItem
+  }
+}
